1c2741ffb624d97bbd6300f76831675c722566b8
pps-admin <pps.github@gmail.com>
2025-05-30 01:34:47 +0900

    Update: path 'generator/pure_random.py' - Updated by 'nflight11'

---
+++ generator/pure_random.py
@@ -0,0 +1,13 @@
+import sys
+import random
+import hashlib
+
+from random import randint as rnd
+
+# Fixed Random Seed
+hashValue = hashlib.sha256("|".join(sys.argv[1:]).encode()).hexdigest()
+random.seed(hashValue)
+
+Nl = 1; Nr = 100; nl = 1; nr = 10**18
+
+print(rnd(Nl, Nr), rnd(nl, nr))
